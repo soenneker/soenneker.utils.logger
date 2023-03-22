fa7f0bd6c1cde5b606fa3fba4bb76a6cabc7715a
Jake Soenneker <jake@soenneker.com>
2023-03-21 19:52:19 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Utils.Logger.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Utils.Logger/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.utils.logger/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.utils.logger/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Utils.Logger.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Utils.Logger/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Utils.Logger
+### A useful utility library dealing with Serilog logging
+
+## Installation
+
+```
+Install-Package Soenneker.Utils.Logger
+```
---
+++ src/LoggerUtil.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
+using Soenneker.Extensions.String;
+
+namespace Soenneker.Utils.Logger;
+
+/// <summary>
+/// A useful utility library dealing with Serilog logging
+/// </summary>
+public static class LoggerUtil
+{
+    private static LoggingLevelSwitch? _loggingLevelSwitch;
+
+    public static void Init()
+    {
+        _loggingLevelSwitch = new LoggingLevelSwitch(LogEventLevel.Verbose);
+    }
+
+    /// <summary>
+    /// Uses the static Serilog Log.Logger, and returns Microsoft ILogger after building a new one. Avoid if you can, utilize DI!
+    /// Serilog should be configured with applicable sinks before calling this
+    /// </summary>
+    public static ILogger<T> BuildLogger<T>()
+    {
+        ILogger<T> logger = new SerilogLoggerFactory(Log.Logger).CreateLogger<T>();
+
+        return logger;
+    }
+
+    public static LoggingLevelSwitch GetSwitch()
+    {
+        if (_loggingLevelSwitch == null)
+            throw new NullReferenceException("Make sure to call Init() before getting the log level switch");
+
+        return _loggingLevelSwitch;
+    }
+
+    public static LogEventLevel GetLogEventLevelFromConfigRoot(IConfigurationRoot configRoot)
+    {
+        LogEventLevel? logEventLevel = configRoot.GetValue<string>("Log:DefaultLogLevel").TryToEnum<LogEventLevel>();
+
+        if (logEventLevel == null)
+            logEventLevel = LogEventLevel.Verbose;
+
+        return logEventLevel.Value;
+    }
+
+    public static LogEventLevel SetLogLevelFromConfigRoot(IConfigurationRoot configRoot)
+    {
+        LogEventLevel switchLevel = GetLogEventLevelFromConfigRoot(configRoot);
+
+        return SetLogLevel(switchLevel);
+    }
+
+    public static LogEventLevel SetLogLevel(LogEventLevel logEventLevel)
+    {
+        _loggingLevelSwitch!.MinimumLevel = logEventLevel;
+
+        return logEventLevel;
+    }
+}
